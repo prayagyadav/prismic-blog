ad7d84ff479f6424aa2e86b8afd0204d05f542bf
prayagyadav <prayagyadav206@gmail.com>
2024-04-01 04:28:30 +0530

    Automated Commit

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 
+export const dynamic = 'force-dynamic';
 export async function POST() {
   revalidateTag("prismic");
 
